a9e6c4f134bb6e6325cb50f467a3eaefe854c346
RJ Goetz <rj@rjgoetz.com>
2017-03-10 15:30:16 -0600

    Added initial sass partials

--- views/layout.php
+++ views/layout.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Cherish your family moments.</title>
+  <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
   <?php require_once('routes/index.php'); ?>
