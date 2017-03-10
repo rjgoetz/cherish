7d3fdc42f5f92d9b3bb3e34dee04c6807858c890
RJ Goetz <rj@rjgoetz.com>
2017-03-10 11:54:20 -0600

    Initial project setup and directories

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Cherish your family moments.</title>
+</head>
+<body>
+  <h1>Hello Cherish</h1>
+  <p>Your family moments.</p>
+  <ul>
+    <li>Photos</li>
+    <li>Family</li>
+    <li>Share</li>
+  </ul>
+</body>
+</html>
