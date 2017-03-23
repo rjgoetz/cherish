3325f64b0dc4ef92d6f3118d99064981ccda92cc
RJ Goetz <rj@rjgoetz.com>
2017-03-23 09:01:26 -0500

    syntax error

--- models/db.php
+++ models/db.php
@@ -1,7 +1,7 @@
 <?php
 
 if (getenv('env') === 'production') {
-  $dbc = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'bb57ea3b75eb99', '5510cfa0', 'heroku_b9761dfb1ecf620')
+  $dbc = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'bb57ea3b75eb99', '5510cfa0', 'heroku_b9761dfb1ecf620');
 } else {
   $dbc = mysqli_connect('localhost', 'root', 'root', 'cherish');
 }
